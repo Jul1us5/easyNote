1faab68d6193838729bc29893b969fbecc69d7ca
Julius <julius.vlasovas@gmail.com>
2021-03-10 23:38:23 +0200

    refac...

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(express.static(__dirname + '/'));
+
 
 //create a logger
 const logger = winston.createLogger({
