b2cbe62b2c35c50fe5c9db9bb9be0ccb2e9c7c18
Ju1u5 <julius.vlasovas@gmail.com>
2021-03-12 21:30:40 +0200

    -dir ...

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/style/'));
+app.use(express.static(__dirname + '/'));
 
 
 //create a logger
